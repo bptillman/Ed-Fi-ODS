ee62c1303819048ffb9f9d8d512d9ffb3ab795a1
Ben Tillman <ben@headspring.com>
2022-03-10 14:56:20 -0600

    fix version parsing and passing to dotnet

--- build.ps1
+++ build.ps1
@@ -3,6 +3,6 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-& dotnet build .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.sln --configuration Release -p:AssemblyVersion=$env:version -p:FileVersion=$env:version -p:InformationalVersion=$env:informationalVersion
+& dotnet build .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.sln --configuration Release -p:AssemblyVersion=$env:informationalVersion.$env:version -p:FileVersion=$env:informationalVersion.$env:version -p:InformationalVersion=$env:informationalVersion
 & dotnet test .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.sln --configuration Release --no-build --verbosity normal
-& dotnet pack .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.csproj --configuration Release --no-build --verbosity normal -p:VersionPrefix=$env:version -p:NoWarn=NU5123 -p:PackageId=EdFi.Suite3.Admin.DataAccess
+& dotnet pack .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.csproj --configuration Release --no-build --verbosity normal -p:VersionPrefix=$env:informationalVersion.$env:version -p:NoWarn=NU5123 -p:PackageId=EdFi.Suite3.Admin.DataAccess
