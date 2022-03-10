a1b8e71082022bf53cd5c9726df0522c002031e1
Ben Tillman <ben@headspring.com>
2022-03-10 16:17:51 -0600

    Fill out build script more and update yml to use it

--- build.ps1
+++ build.ps1
@@ -3,6 +3,93 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+[CmdLetBinding()]
+param(
+    # Command to execute, defaults to "Build".
+    [string]
+    [ValidateSet("Clean", "Build", "Test")]
+    $Command = "Build",
+
+    [switch] $SelfContained,
+
+    # Informational version number, defaults 1.0
+    [string]
+    $InformationalVersion = "1.0",
+
+    # Build counter from the automation tool.
+    [string]
+    $BuildCounter = "1",
+
+    # .NET project build configuration, defaults to "Release". Options are: Debug, Release.
+    [string]
+    [ValidateSet("Debug", "Release")]
+    $Configuration = "Release"
+)
+
+$solution = "Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.sln"
+Import-Module -Name ("$PSScriptRoot/eng/build-helpers.psm1") -Force
+$version = "$InformationalVersion.$BuildCounter"
+
 & dotnet build .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.sln --configuration Release -p:AssemblyVersion=$env:informationalVersion.$env:version -p:FileVersion=$env:informationalVersion.$env:version -p:InformationalVersion=$env:informationalVersion
 & dotnet test .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.sln --configuration Release --no-build --verbosity normal
 & dotnet pack .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.csproj --configuration Release --no-build --verbosity normal -p:VersionPrefix=$env:informationalVersion.$env:version -p:NoWarn=NU5123 -p:PackageId=EdFi.Suite3.Admin.DataAccess
+
+
+function Invoke-Execute {
+    param (
+        [ScriptBlock]
+        $Command
+    )
+
+    $global:lastexitcode = 0
+    & $Command
+
+    if ($lastexitcode -ne 0) {
+        throw "Error executing command: $Command"
+    }
+}
+
+function Invoke-Step {
+    param (
+        [ScriptBlock]
+        $block
+    )
+
+    $command = $block.ToString().Trim()
+
+    Write-Host
+    Write-Host $command -fore CYAN
+
+    &$block
+}
+
+function Clean {
+    Invoke-Execute { dotnet clean $solution -c $Configuration --nologo -v minimal }
+}
+
+function Compile {
+    Invoke-Execute {
+        dotnet --info
+        dotnet build $solution -c $Configuration -p:AssemblyVersion=$version -p:FileVersion=$version -p:InformationalVersion=$InformationalVersion
+    }
+}
+function Invoke-Build {
+    Write-Host "Building Version $version" -ForegroundColor Cyan
+    Invoke-Step { Clean }
+    Invoke-Step { Compile }
+    Invoke-Step { Test }
+}
+
+function Invoke-Tests {
+    Invoke-Execute { dotnet test $solution  -c $Configuration --no-build -v normal }
+}
+
+Invoke-Main {
+    switch ($Command) {
+        Clean { Invoke-Clean }
+        Build { Invoke-Build }
+        Test { Invoke-Tests }
+        Pack { Invoke-Pack }
+        default { throw "Command '$Command' is not recognized" }
+    }
+}
