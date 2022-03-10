8e8d478a1d2c4e727ed542dcccb49fc3cebdf70f
Ben Tillman <ben@headspring.com>
2022-03-10 14:36:24 -0600

    Add powershell build file and call it from action

---
+++ build.ps1
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+& dotnet build .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.sln --configuration Release -p:AssemblyVersion=$env:version -p:FileVersion=$env:version -p:InformationalVersion=$env:informationalVersion
+& dotnet test .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.sln --configuration Release --no-build --verbosity normal
+& dotnet pack .\Application\EdFi.Admin.DataAccess\EdFi.Admin.DataAccess.csproj --configuration Release --no-build --verbosity normal -p:VersionPrefix=$env:version -p:NoWarn=NU5123 -p:PackageId=EdFi.Suite3.Admin.DataAccess
