57da5a939fd6468cdac957873e1ff4628131d485
Ben Tillman <ben.tillman@accenture.com>
2022-06-27 17:30:49 -0500

    Test removing workflow dispatch

--- Utilities/SdkGen/EdFi.SdkGen.Console/OpenApiCodeGenCliRunner.cs
+++ Utilities/SdkGen/EdFi.SdkGen.Console/OpenApiCodeGenCliRunner.cs
@@ -22,6 +22,7 @@ namespace EdFi.SdkGen.Console
         private const string Identity = "Identity";
         private const string Java = "java";
         private const string All = "All";
+        private const string Alll = "Alll";
         private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private readonly Options _options;
        
